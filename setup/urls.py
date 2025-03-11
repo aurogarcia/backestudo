4d9a282931533859bc7fc994f56856dead6ae15f
aurogarcia <aurogarcia833@gmail.com>
2025-03-10 21:06:29 -0300

    .html

--- setup/urls.py
+++ setup/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from todos.views import home
-path('', home'),
+path('', home),
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return return render(request,'todos/home.html');
+    return render(request,'todos/home.html');
 
